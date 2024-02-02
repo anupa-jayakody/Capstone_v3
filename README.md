d5a6b7c3d48c9cf371a3880a7dcf73c88e062987
Anupa Jayakody <64361830+anupa-jayakody@users.noreply.github.com>
2024-02-02 02:12:17 -0500

    v3 folder 2.2

---
+++ README.md
@@ -0,0 +1,123 @@
+# Capstone
+
+Project Overview new
+
+
+The end goal of the project is to to satisfy your hunger buds by giving  a recipe within a few seconds to match your need.  
+The scope of the project will be to develop a recipe recommender system that will help the user choose a recipe that they could try on their own. 
+
+Problem Area  
+
+With the vast majority of the people being non-vegans, most of the recipe content found on different channels/sites are not vegan friendly. Hence vegans usually strugggle to find recipes tailored to thier liking and often end up needing to find substitutes to prepare food on thier own with no proper following, which is not a convenient solution when they are in need of a quick preparation.
+More importantly, with everyone seeking easy get away solutions, people need a quick solution to a recipe when they plan to prepare food. 
+
+So the project will focus on delivering 2 solutions;  
+
+1. Recommending  a recipe  (Vegan data can be filtered out as well)
+2. Recommending  a recipe based on the ingredients you have on hand or you need to be included in the recipe 
+3. Recommending  a recipe based on keywords such as the cuisine, type of recipe etc. 
+
+My original scope was to only give vegan recommendations, but with the dataset changing,  I was able to widen the scope for generic plus vegan recipes. 
+
+Data Science approach to develop this solution will be:  
+
+Solution 1
+
+1. User will input a n number of ingredients that they have on hand right now or the ingredients that they specifically want to be included in the recipe. 
+2. Solution will check the database of recipes and match the available reicpes' ingredients with the input ingredients.
+3. It will then recommend a specific number of recipes to the user based on the similarity between the input ingredients and the database reicpe ingredients using an approproate similarity matrix.
+4. User then can select the recipe that they like and follow the steps to prepare it and/or refer to the web link to get more details
+
+
+Solution 2
+
+1. User will input a keyword to search for in the recipe in the recipe. 
+2. Solution will check the database of recipes and match the available reicpes' keywords/category/name with the input key.
+3. It will then recommend a specific number of recipes to the user based on the similarity between the input ingredients and the database reicpe ingredients using an approproate similarity matrix.
+4. User then can select the recipe that they like and follow the steps to prepare it and/or refer to the web link to get more details
+
+Impact of the solution  
+
+This will enable any individual to find a recipe within 1-2  mins to their liking, to suit their ingredent preferences or what they have on hand making it convinient to find a recipe matching to thier need at that point. This will not provide generic recommendations to which the individual might not have any ingredient readily available. 
+By giving a recommendation considering above, he will not need to read through the entire recipe first to decide whether it will be a good try for him at that point as the recommendations will be given based on his preferences. 
+
+Dataset  
+
+Capstone scope of the project will develop the solution based on a dataset of ~520000 recipes extracted from food.com.(kaggle) So the solution developed will be from this dataset.
+[But I wish to extend this to a live extracting of recipes so that the recipe database will be much wider. I also plan to add an approximate cost for each recipe based on the market cost for each ingredient so that that the user can refine their selection based on the budget criteria as well.] 
+
+Dataset includes below columns  
+
+- Recipe title
+- Ingredients
+- Preparation method
+- Recipe URL
+
+Below are the processes followed to conduct the preliminary analysis of the dataset and to do an EDA.  
+
+Dataset checking  
+
+
+- Summary and types of data 
+- Count
+- Duplicates
+- Null/missing values 
+
+
+EDA  
+
+- Most common ingredients by frequency of usage in recipes
+- Most common recipe types by the title words
+- Length of recipes by ingredient count
+- Length of recipes by title count
+- Correlation between length of titles and ingredients 
+- Recipe rating distribution
+- Correlation of different nutrition contents
+- Distribution of recipes by nutrition contents 
+- Vegan recipes distribution
+
+
+Pre-Processing data  
+
+When analysing data, it was visible that the ingredients column has many words that are not real ingredients. Since this is the column we wil use for our modeling, I removed those unnecessary words for the analysis. Techniques used here are; 
+
+
+- Tokenization
+- Lemmatization
+- Removing stop words
+- Removing measuring units
+- Removing symbols/characters/numbers
+- Lower casing
+
+In Sprint 1, I seperated each ingredient to individual tokens and analysed, however it was visible that some ingredients need to be maintained in the original form such as 'red pepper', 'bell pepper'. Therefore, I improved my pre processing steps to incorporate this. Further more, one function was created combining all the steps. 
+  
+
+
+Modeling
+
+ Below are the steps used
+ 
+- Use Word2Vec, TFIDF vectorization for model comparison
+- Calculating mean embedding for each ingredient 
+- Calculating the mean embedding for the input ingredient
+- Map the embeddings of different words to check if it is sensible
+- Enter random keywords to get the most similar ingredients 
+- Calculating the cosine similarity between the input and the recipe
+- Sorting the results by the scores 
+- Select the best model
+- Finetune the hyperparameters
+
+I selected the Word2Vec model here for my next steps. Both Pre-processing and Modeling steps were done for both search options (Ingredient and Keywords) 
+
+
+Productization
+
+
+Next steps
+- Give a URL to directly connect with the source recipe for the user to follow along easily
+- Convert the datasource to a dynamic API pulling data from web
+- 
+- Create a mobile application
+
+
+
---
+++ Streamlit/app.py
@@ -0,0 +1,569 @@
+import streamlit as st
+import streamlit.components.v1 as stc
+
+#load libraries
+
+#importing the basic libraries
+import numpy as np
+import pandas as pd
+import ast
+
+
+
+ #importing the regex library
+import re
+
+#importing the nltk library
+import nltk
+
+
+#lemmatizer
+from nltk.stem import WordNetLemmatizer
+
+#  nltk stopwords
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+#defining the stop words
+ENGLISH_STOP_WORDS = stopwords.words('english')
+
+
+
+#library for word embedding
+from gensim.models import Word2Vec
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize import word_tokenize
+from collections import defaultdict
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+
+
+#load data
+
+@st.cache_data
+def load_data():
+
+
+    recipes = pd.read_csv('C:/Users/e312995/OneDrive - WESCO DISTRIBUTION/Documents/PERSONAL/BRAINSTATION/CAPSTONE-GIT/Capstone_New/Docs/recipes_streamlit.csv')
+    return recipes
+
+
+#parser ingredients input.
+
+@st.cache_data
+def parser(input_keys):
+
+    remove_= { 'oil', 'salt', 'pepper'}
+
+    #defining measuring units
+
+    measuring_words= ['ml', 'mL', 'milliliter', 'millilitre', 'cc' , 'cubic centimeter', 'l', 'L', 'liter', 'litre', 'dl', 'dL', 'deciliter', 'decilitre', 'teaspoon', 't' , 'tsp.','tablespoon' , 'T', 'tbl', 'tbs', 'tbsp', 'fluid ounce', 'fl oz',  'gill', 'cup',  'c', 'pint', 'p', 'pt', 'fl pt','quart', 'q', 'qt', 'fl qt', 'gallon' , 'g' , 'gal' , 'g', 'milligram', 'milligramme', 'g' , 'gram' , 'gramme', 'kg','kilogram', 'kilogramme', 'pound', 'lb', 'ounce', 'oz', 'mm', 'milimeter', 'millimetre', 'cm' , 'centimeter', 'centimetre', 'm' , 'meter','metre', 'inch', 'in', 'yard', '°C' , 'degree celsius','°F' ,'Farenheit', 'tsp']
+
+    ingredients_cleaned = []
+    lemmatizer = WordNetLemmatizer()
+    
+    # for ingredients_list in input_keys:
+        
+    ingredients_words = re.split(',', input_keys)
+
+    
+    for ingredient in ingredients_words:
+        items= ingredient.split()
+        items = [lemmatizer.lemmatize(word.lower()) for word in items
+                    if word.isalpha() and 
+                    word.lower() not in ENGLISH_STOP_WORDS and 
+                    word.lower() not in measuring_words and 
+                    word.lower() not in remove_]
+        
+        if items:
+            ingredients_cleaned.append(' '.join(items))
+        #ingredients_cleaned.append(' '.join(items))
+    
+
+    return ingredients_cleaned
+
+
+
+
+#parser ingredients
+@st.cache_data
+def parser_ing(input_keys):
+
+    # Defining measuring units
+    remove_ = {'oil', 'salt', 'pepper'}
+
+
+    # Defining measuring units
+    measuring_words = ['ml', 'mL', 'milliliter', 'millilitre', 'cc', 'cubic centimeter', 'l', 'L', 'liter', 'litre', 'dl',
+                   'dL', 'deciliter', 'decilitre', 'teaspoon', 't', 'tsp.', 'tablespoon', 'T', 'tbl', 'tbs', 'tbsp',
+                   'fluid ounce', 'fl oz', 'gill', 'cup', 'c', 'pint', 'p', 'pt', 'fl pt', 'quart', 'q', 'qt', 'fl qt',
+                   'gallon', 'g', 'gal', 'g', 'milligram', 'milligramme', 'g', 'gram', 'gramme', 'kg', 'kilogram',
+                   'kilogramme', 'pound', 'lb', 'ounce', 'oz', 'mm', 'milimeter', 'millimetre', 'cm', 'centimeter',
+                   'centimetre', 'm', 'meter', 'metre', 'inch', 'in', 'yard', '°C', 'degree celsius', '°F', 'Farenheit', 'tsp']
+
+
+    cleaned_ingredients_all_recipes = []
+
+
+    for each_ingredient_list in input_keys:
+        comma_list = re.split(',', each_ingredient_list)  # splitting the ingredients by commas
+
+
+        cleaned_ingredients = []  # new list to store cleaned ingredients for a single recipe
+
+
+        lemmatizer = WordNetLemmatizer()  # lemmatize
+
+
+        for each_word_set in comma_list:
+            items = [word.lower() for word in re.findall(r'\b\w+\b', each_word_set)]  # Extract individual words and convert to lowercase
+
+
+            items = [word for word in items if word.isalpha()]  # filtering only letters
+
+
+            
+
+
+            items = [lemmatizer.lemmatize(word) for word in items]  # lemmatizing
+
+
+            items = [word for word in items if word not in ENGLISH_STOP_WORDS]  # removing stop words
+
+
+            items = [word for word in items if word not in measuring_words]  # removing measuring words
+
+
+            items = [word for word in items if word not in remove_]
+
+
+            if items:
+                cleaned_ingredients.extend(items)
+
+
+        cleaned_ingredients_all_recipes.append(cleaned_ingredients)
+
+    return(cleaned_ingredients_all_recipes)
+
+
+
+
+
+#parser
+@st.cache_data
+def model():
+    
+    phrases_model= Word2Vec.load('C:/Users/e312995/OneDrive - WESCO DISTRIBUTION/Documents/PERSONAL/BRAINSTATION/CAPSTONE-GIT/CAPSTONE_NEW/Models/phrases_model_new(op1).bin')
+        
+    return phrases_model #return the list
+
+
+#parser recommender
+@st.cache_data
+def recommender(user_input):
+
+    print("before paser : ",user_input)
+    print("type : ",type(user_input))
+
+    #user_input = ','.join(map(str, user_input)) #converting to str, mapping and joining by the ','
+    
+    user_input.split(',')
+    print('after split:', user_input)
+
+    user_input_2=parser(user_input)
+    
+    print("after paser : ",user_input_2)
+    print("type : ",type(user_input_2))
+
+
+
+    recipes= load_data()
+    
+    phrases_model= model()
+    print('model')
+    
+    ingredients= recipes['RecipeIngredientParts']
+    print('before parser:', ingredients[0])
+    
+    
+    
+
+    ingredients_cleaned = parser_ing(ingredients)
+    print(ingredients_cleaned[0])
+
+    
+
+
+# mean Embeddings for User Input
+    user_mean_vector =np.mean([phrases_model.wv[ingredient] for ingredient in user_input_2 if ingredient in phrases_model.wv] or [np.zeros(200)], axis=0)
+
+
+
+# check if user_mean_vector contains NaN values
+    if np.isnan(user_mean_vector).any():
+        print("User input vectors contain NaN values.")
+
+ 
+    else:
+
+    # mean ingredient vectors for each recipe
+        recipe_vectors = [np.mean([phrases_model.wv[sub_ingredient] for sub_ingredient in ingredient if sub_ingredient in phrases_model.wv] or [np.zeros(200)], axis=0) for ingredient in ingredients_cleaned]
+
+
+
+
+
+    # Check if any recipe vector contains NaN values
+        if not recipe_vectors:
+            print('nothing obtained for recipe vectors')
+
+        else:
+        
+        # cosine similarity between user mean vector and recipe mean vectors
+            cosine_similarities = cosine_similarity([user_mean_vector], recipe_vectors)
+
+
+        # indices of top N most similar recipes
+            top_recipes = np.argsort(cosine_similarities[0])[::-1][:5]
+
+
+
+            recommendation= pd.DataFrame( columns=['title','ingredients', 'category','calories', 'time', 'score'] ) #dataframe with columns
+
+    
+            for i in top_recipes: #defining the data for each recommendation
+
+            
+                title = recipes['Name'].iloc[i]
+                calories = '{:.0f}'.format(recipes['Calories'].iloc[i])
+                time= recipes['TotalTime'].iloc[i]
+                category = recipes['RecipeCategory'].iloc[i]
+                ingredients = recipes['RecipeIngredientParts'].iloc[i]
+                score= '{:.0f}'.format(cosine_similarities[0][i])
+                #instructions= recipes['RecipeInstructions'].iloc[i]
+                recommendation = recommendation.append(
+                                {'title': title, 
+                                'ingredients': ingredients,
+                                'category' : category, 
+                                'calories' : calories,
+                                #'instructions': instructions,
+                                'time' : time, 
+                                'score': score},
+                                     ignore_index=True)
+                
+    return recommendation  
+
+
+
+
+    
+    
+
+#recommending logic
+
+import streamlit as st
+import requests
+from streamlit_lottie import st_lottie
+
+left_column, right_column = st.columns(2)
+
+with left_column:
+
+    st.title('Flavour Fuze')
+    st.subheader('Elevate your Culinary experience with Flavour Fuze - where every dish becomes and adventure')
+    st.caption('By Anupa Jayakody')
+
+with right_column:
+
+    lottiefile = requests.get('https://lottie.host/dc1b8d65-0ec5-4ca6-9959-c35c8842809b/picTTTEeVt.json', verify=False)
+    st_lottie(lottiefile.json())
+
+
+st.divider()
+st.subheader('Search by Ingredients')
+
+
+
+#user input
+
+user_ingredients= st.text_input('Do you have some ingredients and you want to search the recipes that you can make them??')
+
+if st.button('Lets ing-in !!'):
+
+    #recipes= load_data('C:/Users/e312995/OneDrive - WESCO DISTRIBUTION/Documents/PERSONAL/BRAINSTATION/CAPSTONE/Datasets/recipe_sample_3.csv')
+
+    loading_image= st_lottie(requests.get('https://lottie.host/9e4a2516-2bd2-4f3a-97dc-e13a1c8b17d3/WsU810K1aZ.json',verify=False).json())
+
+
+    recommend_recipes= recommender(user_ingredients)
+    
+    if recommend_recipes is not None and not recommend_recipes.empty:
+        
+        st.table(recommend_recipes)
+    
+    else:
+
+        st.write('no recommendations today')
+
+
+
+
+
+
+### KEYWORD MODEL
+        
+
+#load data
+@st.cache_data
+def load_model_text():
+
+    phrases_model= Word2Vec.load('C:/Users/e312995/OneDrive - WESCO DISTRIBUTION/Documents/PERSONAL/BRAINSTATION/CAPSTONE-GIT/CAPSTONE_NEW/Models/phrases_model_new(op2).bin')
+        
+    return phrases_model #return the list
+
+
+
+
+#parser text input
+
+@st.cache_data
+def parser_text(input_keys): #function
+
+    # Defining measuring units
+    remove_ = {'oil', 'salt', 'pepper'}
+
+
+    cleaned_ingredients_all_recipes = []
+
+
+    for each_ingredient_list in input_keys:
+        comma_list = re.split(',', each_ingredient_list)  # splitting the ingredients by commas
+
+
+        cleaned_ingredients = []  # new list to store cleaned ingredients for a single recipe
+
+
+        lemmatizer = WordNetLemmatizer()  # lemmatize
+
+
+        for each_word_set in comma_list:
+            items = [word.lower() for word in re.findall(r'\b\w+\b', each_word_set)]  # Extract individual words and convert to lowercase
+
+
+            items = [word for word in items if word.isalpha()]  # filtering only letters
+
+
+            
+
+
+            items = [lemmatizer.lemmatize(word) for word in items]  # lemmatizing
+
+
+            items = [word for word in items if word not in ENGLISH_STOP_WORDS]  # removing stop words
+
+
+            # removing measuring words
+
+
+            items = [word for word in items if word not in remove_]
+
+
+            if items:
+                cleaned_ingredients.extend(items)
+
+
+        cleaned_ingredients_all_recipes.append(cleaned_ingredients)
+
+    return(cleaned_ingredients_all_recipes)
+
+
+
+
+
+#parser user text 
+
+@st.cache_data
+def parser_user_text(input_keys):
+
+
+#defining measuring units
+
+    measurment_url= 'https://en.wikibooks.org/wiki/Cookbook:Units_of_measurement' #data source
+
+
+    measuring_words= ['ml', 'mL', 'milliliter', 'millilitre', 'cc' , 'cubic centimeter', 'l', 'L', 'liter', 'litre', 'dl', 'dL', 'deciliter', 'decilitre', 'teaspoon', 't' , 'tsp.',
+'tablespoon' , 'T', 'tbl', 'tbs', 'tbsp', 'fluid ounce', 'fl oz',  'gill', 'cup',  'c', 'pint', 'p', 'pt', 'fl pt',
+'quart', 'q', 'qt', 'fl qt', 'gallon' , 'g' , 'gal' , 'g', 'milligram', 'milligramme', 'g' , 'gram' , 'gramme', 'kg',
+'kilogram', 'kilogramme', 'pound', 'lb', 'ounce', 'oz', 'mm', 'millimeter', 'millimetre', 'cm' , 'centimeter', 'centimetre', 'm' , 'meter',
+'metre', 'inch', 'in', 'yard', '°C' , 'degree celsius','°F' ,'Farenheit', 'tsp']
+    
+    cleaned_ingredients_all_recipes = []
+
+
+    #for each_ingredient_list in input_keys:
+    comma_list = re.split(',', input_keys)
+    
+    #comma_list = re.split(' ', each_ingredient_list)   # splitting the ingredients by commas
+
+
+    cleaned_ingredients = []  # new list to store cleaned ingredients for all  recipes
+
+
+    lemmatizer = WordNetLemmatizer()  # lemmatize
+
+
+    for each_word_set in comma_list:
+        items = [word.lower() for word in re.findall(r'\b\w+\b', each_word_set)]  # extract individual words and convert to lowercase
+        print('lower:', items)
+
+
+        items = [word for word in items if word.isalpha()]  # filtering only letters
+        print('alpha list:', comma_list)
+
+
+        items = [lemmatizer.lemmatize(word) for word in items]  # lemmatizing
+
+
+        items = [word for word in items if word not in ENGLISH_STOP_WORDS]  # removing stop words
+
+
+        if items:
+            cleaned_ingredients.extend(items)
+            print('one before last:',cleaned_ingredients )
+
+
+    #cleaned_ingredients_all_recipes.append(cleaned_ingredients)
+
+    return(cleaned_ingredients)
+
+
+
+
+
+#recommender
+
+@st.cache_data
+def recommender_text(text_input):
+
+    recipes= load_data()
+    
+    phrases_model= load_model_text()
+    print('model')
+
+    text_input.split(',')
+    print('after split:', text_input)
+    
+
+    user_input= parser_user_text(text_input)
+    print('after parser user text:', user_input )
+    
+
+    # mean Embeddings for User Input
+    user_mean_vector =np.mean([phrases_model.wv[user_text] for user_text in user_input if user_text in phrases_model.wv] or [np.zeros(200)], axis=0)
+
+
+
+    #text data
+
+    text_data= recipes['text_data']
+    print('before parser:', text_data)
+
+    #text_data_cleaned= parser_text(text_data)
+    
+
+    text_data_cleaned= recipes['TextDataCleaned']
+
+    print('after parser:', text_data_cleaned[0])
+    
+
+
+
+
+    # check if user_mean_vector contains NaN values
+    if np.isnan(user_mean_vector).any():
+        print("User input vectors contain NaN values.")
+
+
+    else:
+
+        # mean ingredient vectors for each recipe
+        text_vectors = [np.mean([phrases_model.wv[sub_text] for sub_text in text if sub_text in phrases_model.wv] or [np.zeros(200)], axis=0) for text in text_data_cleaned]
+
+
+
+
+
+        # Check if any recipe vector contains NaN values
+        if not text_vectors:
+            print('nothing obtained for recipe vectors')
+
+        else:
+            
+            # cosine similarity between user mean vector and recipe mean vectors
+            cosine_similarities_text= cosine_similarity([user_mean_vector], text_vectors)
+
+
+            # indices of top N most similar recipes
+            top_recipes = np.argsort(cosine_similarities_text[0])[::-1][:5]
+
+
+
+            recommendation= pd.DataFrame( columns=['Name','Ingredients', 'Category', 'Keywords', 'Calories', 'Time', 'Score'] ) #dataframe with columns
+
+    
+            for i in top_recipes: #defining the data for each recommendation
+
+            
+                title = recipes['Name'].iloc[i]
+                calories = '{:.0f}'.format(recipes['Calories'].iloc[i])
+                time= recipes['TotalTime'].iloc[i]
+                ingredients = recipes['RecipeIngredientParts'].iloc[i]
+                category = recipes['RecipeCategory'].iloc[i]
+                keywords = recipes['Keywords'].iloc[i]
+                score= '{:.0f}'.format(cosine_similarities_text[0][i])
+                recommendation = recommendation.append(
+                                {'Name': title, 
+                                'Ingredients': ingredients, 
+                                'Category': category,
+                                'Keywords': keywords,
+                                'Calories' : calories, 
+                                'Time' : time, 
+                                'Score': score}, 
+                                     ignore_index=True)
+
+
+
+
+    return recommendation
+
+       
+
+
+
+
+
+
+#recommending logic
+
+st.divider()
+st.subheader('Search by Keywords')
+
+#user input
+
+keyword_input= st.text_input('Do you want to search by a keyword today??')
+
+if st.button('Lets key-in !!'):
+
+    loading_image= st_lottie((requests.get('https://lottie.host/9e4a2516-2bd2-4f3a-97dc-e13a1c8b17d3/WsU810K1aZ.json',verify=False).json()), height=100, width=100 , )
+
+    recommend_recipes= recommender_text(keyword_input)
+    
+    if recommend_recipes is not None and not recommend_recipes.empty:
+
+        loading_image.empty()
+        st.table(recommend_recipes)
+    
+    else:
+
+        st.write('no recommendations today')
